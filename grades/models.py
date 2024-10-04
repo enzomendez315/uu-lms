60ef5d44fa2eb230520072595f5a7a13bb1ba3a7
Enzo Mendez <enzo.mendez315@gmail.com>
2024-10-04 04:16:45 -0600

    Refactoring header.

--- grades/models.py
+++ grades/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 
-# TODO: Add blanks, nulls, and defaults
-# TODO: Revise on_deletes
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
