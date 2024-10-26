44063d611c0fb948184162b9fac55c4a1420a21f
Enzo Mendez <enzo.mendez315@gmail.com>
2024-10-26 17:36:07 -0600

    Finished phase 2.

--- grades/views.py
+++ grades/views.py
@@ -4,32 +4,24 @@ from . import models
 from django.http import Http404
 
 def index(request):
-    # Get all assignments
     assignments = models.Assignment.objects.all()
-    return render(request, "index.html", {'assignments': assignments})
+    return render(request, "index.html", {"assignments": assignments})
 
 def assignment(request, assignment_id):
     try:
-        # Get assignment by ID
         assignment = models.Assignment.objects.get(id=assignment_id)
-
-        # Get total number of submissions
         submissions_count = assignment.submission_set.count()
-
-        # Get the user object for the grader
         grader = models.User.objects.get(username="g")
 
-        # Get number of submissions assigned to the grader
+        # Get number of submissions assigned to the grader and total number of students
         for_grading_count = assignment.submission_set.filter(grader=grader).count()
-
-        # Get total number of students
         students_count = models.Group.objects.get(name="Students").user_set.count()
 
         additional_info = {
-            'assignment': assignment,
-            'submissions': submissions_count,
-            'for_grading': for_grading_count,
-            'students': students_count
+            "assignment": assignment,
+            "submissions": submissions_count,
+            "for_grading": for_grading_count,
+            "students": students_count
         }
     except models.Assignment.DoesNotExist:
         raise Http404("Page does not exist.")
@@ -37,58 +29,67 @@ def assignment(request, assignment_id):
 
 def submissions(request, assignment_id):
     if request.method == "POST":
+        # Extract the submission ID's and the related objects
+        submission_ids = extract_data(request.POST)
+        submissions = models.Submission.objects.filter(id__in=submission_ids, assignment_id=assignment_id)
+
+        for submission in submissions:
+            id = str(submission.id)
+            value = request.POST[id]
+            score = None
+            if value:
+                score = float(value)
+            submission.score = score
+
+        models.Submission.objects.bulk_update(submissions, ["score"])
         return redirect(f"/{assignment_id}/submissions")
     try:
-        # Get assignment by ID
         assignment = models.Assignment.objects.get(id=assignment_id)
-
-        # Get the user object for the grader
         grader = models.User.objects.get(username="g")
 
         # Get submissions assigned to the grader and sort them by author's username
-        for_grading = assignment.submission_set.filter(grader=grader).select_related('author').order_by('author__username')
+        for_grading = assignment.submission_set.filter(grader=grader).select_related("author").order_by("author__username")
 
         submissions = []
 
         for submission in for_grading:
-            # Get author's name
+            # Get author's name, file url, and score
             author = submission.author.get_full_name()
-
-            # Get file url
             file = submission.file
-
-            # Get score
             score = submission.score
-
             submissions.append([author, file, score])
 
         additional_info = {
-            'assignment': assignment,
-            'submissions': submissions
+            "assignment": assignment,
+            "submissions": submissions
         }
     except models.Assignment.DoesNotExist:
         raise Http404("Page does not exist.")
     return render(request, "submissions.html", additional_info)
 
 def profile(request):
-    # Get all assignments
     all_assignments = models.Assignment.objects.all()
-
-    # Get the user object for the grader
     grader = models.User.objects.get(username="g")
-
     assignments = []
 
     for assignment in all_assignments:
-        # Get number of submissions that have been graded
+        # Get number of submissions that have been graded and number of submissions assigned to the grader
         graded_count = assignment.submission_set.filter(grader=grader, score__isnull=False).count()
-        
-        # Get number of submissions assigned to the grader
         for_grading_count = assignment.submission_set.filter(grader=grader).count()
-
         assignments.append([assignment.id, assignment.title, graded_count, for_grading_count])
-
-    return render(request, "profile.html", {'assignments': assignments})
+    return render(request, "profile.html", {"assignments": assignments})
 
 def login_form(request):
     return render(request, "login.html")
+
+def extract_data(request_data):
+    submissions = []
+
+    # Iterate through request.POST
+    for key in request_data:
+        if key.startswith("grade-"):
+            # Convert ID to an integer
+            id = int(key.removeprefix("grade-"))
+            submissions.append(id)
+            
+    return submissions
